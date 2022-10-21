1acf8f3acf6302ae72cb606f1cde4d3792c5498d
Марк Крылов <mark.krylov93@gmail.com>
2022-10-21 21:25:27 +0600

    review

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -16,16 +16,3 @@ class ReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS
 
-
-class NotAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author != request.user
-        )
