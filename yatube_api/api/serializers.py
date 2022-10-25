13a65433872bd87184924228b4749015fadda989
Марк Крылов <mark.krylov93@gmail.com>
2022-10-25 17:27:35 +0600

    changes

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -51,7 +51,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
-        read_only_fields =('post',)
+        read_only_fields = ('post',)
 
 
 class FollowSerializer(serializers.ModelSerializer):
